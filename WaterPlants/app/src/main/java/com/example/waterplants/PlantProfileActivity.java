469e9023357d7ba9364476852c3edd2e95c5c026
Emanuela Busuioc <busuiocemanuela@yahoo.com>
2019-05-28 21:48:19 +0300

    Flower profile(partially) and timestamp adnotation

---
+++ WaterPlants/app/src/main/java/com/example/waterplants/PlantProfileActivity.java
@@ -0,0 +1,13 @@
+package com.example.waterplants;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlantProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plant_profile);
+    }
+}
